0b5a2bf891fa01adb26b10f2c05141ac3b9dfce8
Cozloschi Luca <125896237+CaineleJoe@users.noreply.github.com>
2025-01-09 03:57:07 +0200

    added database connection logic

--- EncryptedDatabase12/main.py
+++ EncryptedDatabase12/main.py
@@ -1,5 +1,6 @@
 import sys
-
+import mysql.connector
+from mysql.connector import errorcode
 
 def print_usage():
     print("Usage:")
@@ -7,10 +8,19 @@ def print_usage():
     print("  python encrypted_database.py read <file_id>")
     print("  python encrypted_database.py delete <file_id>")
 
+def db_connect():
+    db_connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="bd2024",
+        database="encrypted_database"
+    )
+    return db_connection
 
 def main():
-    if len(sys.argv) < 2 & len(sys.argv) > 3:
-        print("[Error] No command provided.")
+#command validation
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("[Error] Invalid number of arguments.")
         print_usage()
         return
 
@@ -20,7 +30,27 @@ def main():
     if command not in valid_commands:
         print(f"[Error] Unknown command: '{command}'")
         print_usage()
+        return
     else:
         print(f"[OK] Command recognized: {command}")
+
+#Try to connect to the database
+    try:
+        db_connection = db_connect()
+        print("[OK] Successfully connected to MySQL database.")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("[Error] Wrong username or password.")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("[Error] Database does not exist.")
+        else:
+            print(f"[Error] {err}")
+        sys.exit(1)
+
+ #Close the connection
+    if db_connection.is_connected():
+        db_connection.close()
+        print("[OK] Database connection closed.")
+
 if __name__ == "__main__":
     main()
