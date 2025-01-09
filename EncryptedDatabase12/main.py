a7850044e54d3637aab35b3acab08a6aae6dcc90
Cozloschi Luca <125896237+CaineleJoe@users.noreply.github.com>
2025-01-09 05:46:34 +0200

    file adding logic with XOR encryption.

--- EncryptedDatabase12/main.py
+++ EncryptedDatabase12/main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -16,6 +18,46 @@ def db_connect():
         database="encrypted_database"
     )
     return db_connection
+
+def encrypt_file(src_path, dst_path, key=170, chunk_size=256):
+    try:
+        with open(src_path, "rb") as src_file, open(dst_path, "wb") as dst_file:
+            while True:
+                chunk = src_file.read(chunk_size)
+                if not chunk:
+                    break
+                encrypted_chunk = bytes([b ^ key for b in chunk])
+                dst_file.write(encrypted_chunk)
+        print("[OK] File encrypted and saved to:", dst_path)
+    except FileNotFoundError:
+        print(f"[Error] Source file not found: {src_path}")
+    except PermissionError:
+        print(f"[Error] Permission denied. Could not read or write file.")
+    except Exception as e:
+        print(f"[Error] Something went wrong: {e}")
+
+
+
+def handle_add(filepath):
+    if not os.path.isfile(filepath):
+        print(f"[Error] File does not exist {filepath}")
+        return
+    desktop_path=os.path.join(os.path.expanduser("~"), "Desktop")
+    encrypted_folder=os.path.join(desktop_path, "Encrypted")
+    if not os.path.exists(encrypted_folder):
+        try:
+            os.makedirs(encrypted_folder)
+        except Exception as e:
+            print(f"[Error] Failed to create folder {encrypted_folder}. Reason: {e}")
+            return
+
+    base_name=os.path.basename(filepath)
+    encrypted_filename=base_name+"_encrypted"
+    encrypted_filepath=os.path.join(encrypted_folder, encrypted_filename)
+    #actual file encryption
+    encrypt_file(filepath, encrypted_filepath)
+
+
 
 def main():
 #command validation
@@ -46,6 +88,14 @@ def main():
         else:
             print(f"[Error] {err}")
         sys.exit(1)
+
+
+#Command Handler
+    if command=="add":
+        filepath=sys.argv[2]
+        handle_add(filepath)
+
+
 
  #Close the connection
     if db_connection.is_connected():
